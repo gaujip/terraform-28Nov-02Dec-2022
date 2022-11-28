f10be57d075085705748dac225618273a76fac3f
Jeganathan Swaminathan <mail2jegan@gmail.com>
2022-11-28 04:30:24 -0800

    Added Day1 lab exercises.

--- Day1/create-docker-container/main.tf
+++ Day1/create-docker-container/main.tf
@@ -2,20 +2,20 @@ terraform {
    required_providers {
 	docker = {
 		source = "kreuzwerker/docker"
-                version = "2.23.0"
+                version = "=2.23.0"
 	}
    }
 }
 
 provider "docker" {}
 
-resource "docker_image" "nginx" {
+resource "docker_image" "nginx1" {
 	name = "bitnami/nginx:latest"
-	keep_locally = false
+	keep_locally = false 
 }
 
 resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
+  image = docker_image.nginx1.image_id
 
   name = "my-nginx-container"
 
@@ -25,4 +25,3 @@ resource "docker_container" "nginx" {
   }
 
 }
-
