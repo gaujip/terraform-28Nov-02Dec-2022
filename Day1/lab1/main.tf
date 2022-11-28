84332ed95cbef02de1d8afe2127e8cebb6bb30af
Jeganathan Swaminathan <mail2jegan@gmail.com>
2022-11-28 02:07:33 -0800

    Added Day1 lab exercises.

---
+++ Day1/lab1/main.tf
@@ -0,0 +1,28 @@
+terraform {	
+   required_providers {
+	docker = {
+		source = "kreuzwerker/docker"
+                version = "2.23.0"
+	}
+   }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+	name = "bitnami/nginx:latest"
+	keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+
+  name = "my-nginx-container"
+
+  ports {
+	internal = 8080
+	external = 9090
+  }
+
+}
+
